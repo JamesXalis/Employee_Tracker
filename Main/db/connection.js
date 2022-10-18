e0600e97964527446ef91e88b03501e6583c555e
James Xalis <jamesxalis@icloud.com>
2022-10-17 17:15:40 -0700

    added schema, seed, package.json, and connections.

---
+++ Main/db/connection.js
@@ -0,0 +1,13 @@
+const mysql = require ('mysql2')
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    data: '',
+    password: 'Jamesxalis'
+});
+
+db.connect((err) => {
+    if (err) throw err;
+    console.log('Successfully connected to database')
+});
---
+++ Main/db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS registrar_db;
+CREATE DATABASE registrar_db;
+
+USE registrar_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL,
+    manager_id INT,
+    PRIMARY KEY (id)
+);
